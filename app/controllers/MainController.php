bc75912bf6719522294aaadcc44a03f8a9d2895c
renato <renatoc@alunos.utfpr.edu.br>
2018-04-13 17:53:56 -0300

    funcao de enviar email para um unico destino funcionando

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -11,8 +11,12 @@ class MainController extends Controller {
             view("cadastroemail", $lista);
 
 
-        }else
+        }else if(isset($_POST["novo"])) {
             $this->cadastraemail();
+        }else if(isset($_POST["enviar"])){
+            $this->enviaemail();
+        }
+
     }
     public function cadastraemail(){
 	    $novo = new Email();
@@ -20,6 +24,22 @@ class MainController extends Controller {
 	    $novo->setEmail($_POST["novo_email"]);
 	    $novo->save();
 	    redirect("/");
+    }
+    public function enviaemail(){
+	    if(isset($_POST["email_id"])){
+	        $email = new Email();
+	        $email->setId($_POST["email_id"]);
+	        $email = Email::make()->get($email->getId());
+	        dump($email);
+            $mensagem = new Mail();
+            $mensagem->setFrom("testadorwilson@gmail.com");
+            $mensagem->setTo($email->getEmail());
+            $mensagem->setSubject($_POST["assunto"]);
+            $mensagem->setContent($_POST["corpo_email"]);
+            $mensagem->setFromName("Wilson");
+            dump($mensagem);
+            $mensagem->send();
+        }
     }
 
 }
