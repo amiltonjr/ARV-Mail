c2fc69c816c8f82ac5f5b6e953737f64fb87bb2c
renato <renatoc@alunos.utfpr.edu.br>
2018-05-01 21:41:57 -0300

    adicionado o backend de relatorio de emails por dominio

--- app/views/relatorioemail.php
+++ app/views/relatorioemail.php
@@ -5,9 +5,21 @@
     <div class="topo">
         <h2>Lista de emails por domínio</h2>
         <?php
-        foreach($data[1] as $grupo){
-            dump($grupo->getId());
+        $list_consulta_dominio = Email::make()->select('Distinct domain')->find();
+        foreach ($list_consulta_dominio as $key => $domain) {
+            echo '<div class="co  l-3-12">';
+            $dominioatual = $domain->getDomain();
+            echo $dominioatual.'<br><br>';
+            $list_email = Email::make()->all();
+            //dump($list_email);
+            foreach($list_email as $le => $email) {
+                if($email->getDomain() == $dominioatual){
+                    echo $email->getEmail()."<br>";
+                }
+            }
+                echo'<br>';
         }
+
         ?>
     </div>
 
